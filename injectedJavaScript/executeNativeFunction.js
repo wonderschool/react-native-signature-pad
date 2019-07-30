6608b0a4b526b430837e167f4c092df0013d8cae
Clark Cutler <clark@carbonfive.com>
2019-07-30 11:28:14 -0700

    Revert "Add `alert` to help investigate bug"
    This reverts commit 8c56b17cfa072139f9179edf3f32b1c0e87837db.
    
    We cannot reproduce the bug anymore. So we're reverting the alert that
    was added temporarily.

--- injectedJavaScript/executeNativeFunction.js
+++ injectedJavaScript/executeNativeFunction.js
@@ -1,10 +1,6 @@
 var content = `
   function executeNativeFunction(fnName, args) {
-    try {
-      window.location.hash = '&executeFunction<-' + fnName + '&' + '&arguments<-' + JSON.stringify(args) + '&' + (window.ReactNativeWebView || window['ReactABI33_0_0NativeWebView']).postMessage(JSON.stringify(args)) + '&';
-    } catch (error) {
-      alert('' + error + ',' + Object.keys(window).join('   '))
-    }
+    window.location.hash = '&executeFunction<-' + fnName + '&' + '&arguments<-' + JSON.stringify(args) + '&' + window.ReactNativeWebView.postMessage(JSON.stringify(args)) + '&';
   }
 `;
 
